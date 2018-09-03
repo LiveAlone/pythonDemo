f889410e895969333a884e0c871d770c71bfd727
qijunyao <qijunyao@xiaohongshu.com>
2018-09-03 16:12:51 +0800

    add all with orm python config

--- 3.7/metaclass/metadata_object.py
+++ 3.7/metaclass/metadata_object.py
@@ -5,21 +5,26 @@ description: 通过 metadata 实例 -> 创建 Class -> 通过 Class 创建实例
 """
 
 
-# class ListMetaclass(type):
-#     def __new__(cls, name, bases, attrs):
-#         attrs['add'] = lambda self, value: self.append(value)
-#         return type.__new__(cls, name, bases, attrs)
-#
-#
-# class MyList(list, metaclass=ListMetaclass):
-#     pass
-#
-#
-# # 创建MyList.__new__() 创建方式，
-# # 1 准备类创建类对象  2 类名字  3 类继承父类集合  4 类方法集合
-#
+# list 定义 metaclass 类型支持
+class ListMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['add'] = lambda self, value: self.append(value)
+        return type.__new__(cls, name, bases, attrs)
+
+
+class MyList(list, metaclass=ListMetaclass):
+    pass
+
+
+class MyListSub(MyList):
+    pass
+
+
+# append 方法转换成对应的 add 方法， 支持数据的填充
 # L = MyList()
 # L.add(1)
 # print(L)
+
+L = MyListSub()
 
 
