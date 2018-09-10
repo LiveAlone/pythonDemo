dde394ead388ca83cedacb94065253c27bca8c79
qijunyao <qijunyao@xiaohongshu.com>
2018-09-10 12:52:26 +0800

    add all

---
+++ 3.7-fluent/design/metadata_basic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'yaoqijun'
+__mail__ = 'yaoqijunmail@foxmail.com'
+
+'''
+description:  元编程测试方式, 数据属性， 数据处理方式，attribute
+'''
+
+
+
+
+
