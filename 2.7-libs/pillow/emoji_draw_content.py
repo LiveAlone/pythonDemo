fcefc833458d61de5ddc5fc05b93da9caf819cb3
qijunyao <qijunyao@xiaohongshu.com>
2018-10-16 14:32:40 +0800

    add none

--- 2.7-libs/pillow/emoji_draw_content.py
+++ 2.7-libs/pillow/emoji_draw_content.py
@@ -9,6 +9,9 @@ description:
 '''
 from PIL import Image, ImageFont, ImageDraw
 from emojipy import Emoji
+import emoji
+import re
+
 
 # FONT_PATH = "static/DejaVuSansMono.ttf"
 FONT_PATH = "static/Symbola.ttf"
@@ -16,14 +19,16 @@ FONT_PATH = "static/Symbola.ttf"
 
 if __name__ == '__main__':
     # text = u'A\U0001F61FB'
-    text = u'含emoji😁🙂😚😎👤🦏🦒➕➕➕👳'
-    # text = "It's emoji time \u263A \U0001F61C. Let's add some cool " \
-    #           "emotions \U0001F48F \u270C. And some more \u2764 \U0001F436"
-    text = Emoji.unicode_to_image(text)
+    text = u'aa😁🙂😚😎👤🦏🦒➕➕➕👳'
+    # text = u"It's emoji time \u263A \U0001F61C. Let's add some cool " \
+    #        u"emotions \U0001F48F \u270C. And some more \u2764 \U0001F436"
+    # text = Emoji.unicode_to_shortcode(text)
+    text = emoji.get_emoji_regexp().sub(r'', text)
+    text = "Hi emoji null is %s" % text
     print(text)
-    image = Image.new("RGBA", (1500, 500), (255, 255, 255))
-    font = ImageFont.truetype(FONT_PATH, 30, encoding='unic')
-    draw = ImageDraw.Draw(image)
-    draw.text((0, 0), text, (0, 0, 0), font=font)
-    # image.save("Test.png")
-    image.show()
+    # image = Image.new("RGBA", (1500, 500), (255, 255, 255))
+    # font = ImageFont.truetype(FONT_PATH, 30, encoding='unic')
+    # draw = ImageDraw.Draw(image)
+    # draw.text((0, 0), text, (0, 0, 0), font=font)
+    # # image.save("Test.png")
+    # image.show()
